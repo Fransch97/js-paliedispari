07b582d5022ab227c0c3119afc6cfa997bc6d7d2
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-20 14:33:22 +0200

    first idea

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("I'm alive muahahaha, mo son cazz");
+// console.log("I'm alive muahahaha, mo son cazz");
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Bonus: L’inserimento avviene tramite un campo input
+
+
+    const palindroma = "anna";
+    const palindromaLength = palindroma.length - 1;
+    console.log(palindroma.length);
+    const saveAbc = [];
+
+    for(let i = palindromaLength; i >= 0; i--){
+        saveAbc.push(palindroma.charAt(i))
+    }
+    console.log(saveAbc)
+
+
+
+
+
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+// Bonus: L’inserimento avviene tramite un campo input
